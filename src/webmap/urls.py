a0a27ae7aef8eda5379af01c44faaf0edecd0bc8
Timothy Hobbs <timothy@hobbs.cz>
2018-07-12 22:49:41 +0200

    Preset switcher now works

--- src/webmap/urls.py
+++ src/webmap/urls.py
@@ -1,6 +1,7 @@
 """URLs for the webmap app."""
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 
 from webmap import views
 
@@ -13,7 +14,7 @@ urlpatterns = [
     url(r'^leaflet-include.js$', views.LeafletIncludeView.as_view(), name='leaflet_include'),
     url(r'^popup/(?P<pk>\d+)/$', views.PopupView.as_view(), name="popup_view"),
     url(r'^geojson/(?P<layer_slug>[^&]+)/$', views.WebmapGeoJsonView.as_view(), name='data'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if getattr(settings, 'REST_ENABLED', False):
     urlpatterns.append(url(r'^', include(router.urls)))
